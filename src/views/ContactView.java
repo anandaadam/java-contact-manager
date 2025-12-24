96c029d3c8292c87dcf50404408a391990f1fc6a
Adam Ananda Santoso <adamananda02@gmail.com>
2025-12-24 23:49:32 +0700

    feat: create addContact() view method

---
+++ src/views/ContactView.java
@@ -0,0 +1,83 @@
+package views;
+
+import dto.CreateContactRequest;
+import entities.Contact;
+import services.ContactService;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class ContactView {
+    private ContactService contactService;
+    private Scanner scanner = new Scanner(System.in);
+
+    public ContactView(ContactService contactService) {
+        this.contactService = contactService;
+    }
+
+    public void showMenu() {
+        while (true) {
+            System.out.println("=== Contact Manager ===");
+            System.out.println("1. View All Contacts");
+            System.out.println("2. Add Contact");
+            System.out.println("3. Update Contact");
+            System.out.println("4. Remove Contact");
+            System.out.println("5. Add Contact to Bookmark");
+            System.out.println("6. Remove Contact from Bookmark");
+            System.out.println("7. Search Contact");
+            System.out.println("8. View Contacts by Tag");
+            System.out.println("9. View Bookmarked Contacts");
+            System.out.println("10. Done");
+
+            System.out.print("Choose: ");
+            String input = scanner.nextLine();
+
+            switch (input) {
+                case "1" -> viewAllContacts();
+                case "2" -> addContact();
+//                case "3" -> updateContact();
+//                case "4" -> removeContact();
+//                case "5" -> addToBookmark();
+//                case "6" -> removeFromBookmark();
+//                case "7" -> searchContact();
+//                case "8" -> viewByTag();
+//                case "9" -> viewBookmarked();
+                case "10" -> {
+                    System.out.println("Goodbye 👋");
+                    return; //
+                }
+                default -> System.out.println("Invalid choice");
+            }
+        }
+    }
+
+    public void viewAllContacts() {
+        contactService.viewAllContacts();
+    }
+
+    public void addContact() {
+        System.out.println("Add Contact");
+
+        System.out.print("Name: ");
+        String name = scanner.nextLine();
+
+        System.out.print("Phone numbers (comma separated): ");
+        String phoneNumbers = scanner.nextLine();
+
+        System.out.print("Email: ");
+        String email = scanner.nextLine();
+
+        System.out.print("Address: ");
+        String address = scanner.nextLine();
+
+        System.out.print("Tags (comma separated): ");
+        String tags = scanner.nextLine();
+
+        boolean bookmark = false;
+
+        CreateContactRequest request = new CreateContactRequest(name, phoneNumbers, email, address, tags, bookmark);
+        Contact result =  contactService.addContact(request);
+
+        System.out.println(result);
+    }
+}
